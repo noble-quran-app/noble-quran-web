4a7f295114107f1281e6c31fe92cf48fda1c3732
AbdulSamadMalik <samadmalik542762@gmail.com>
2021-05-30 10:26:00 +0530

    updated app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { NetworkService } from './services/network.service';
 import { UpdateService } from './services/update.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'noble-quran-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
