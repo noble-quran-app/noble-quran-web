c17fb93b1e0f01c7440d0dd344c234a437613d86
AbdulSamadMalik <samadmalik542762@gmail.com>
2021-05-31 09:12:59 +0530

    updated splash screen

--- src/app/core/functions.ts
+++ src/app/core/functions.ts
@@ -59,8 +59,8 @@ export const getAyahAudioUrl = (id: number) => {
 };
 
 export const removeElements = () => {
-  if (document.querySelector('.remove-on-app-load')) {
-    const splashNodes = Array.from(document.querySelectorAll('.remove-on-app-load'));
+  if (document.querySelector('.unmount-on-load')) {
+    const splashNodes = Array.from(document.querySelectorAll('.unmount-on-load'));
     splashNodes.forEach((el) => el.parentElement.removeChild(el));
   }
 };
