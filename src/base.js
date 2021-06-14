040b31907bf0431d24f06676b5d1d3d9a937d9aa
AbdulSamadMalik <samadmalik542762@gmail.com>
2021-06-14 13:10:30 +0530

    update base.js

--- src/base.js
+++ src/base.js
@@ -10,7 +10,7 @@ try {
 }
 
 function removeSplash(removeNow) {
-  if (removeNow) {
+  if (removeNow.value) {
     var splashNodes = Array.from(document.querySelectorAll('.splash'));
     for (var index = 0; index < splashNodes.length; index++) {
       if (splashNodes[index]) {
@@ -19,14 +19,13 @@ function removeSplash(removeNow) {
     }
 
     document.dispatchEvent(new CustomEvent('splashcomplete'));
+    document.removeEventListener('routeloaded', removeSplash);
     return;
   }
 
   var currentTime = new Date().getTime();
   var timeGap = 1500 - (currentTime - loadStartedAt);
-  setTimeout(removeSplash, timeGap, true);
+  setTimeout(removeSplash, timeGap, { value: true });
 }
 
-document.addEventListener('routeloaded', function () {
-  removeSplash();
-});
+document.addEventListener('routeloaded', removeSplash);
