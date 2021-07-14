7b4bc63912e542e831c923b6158b281296e6b673
Abdul Samad <samadmalik542762@gmail.com>
2021-07-14 18:36:10 +0000

    Update header component

--- src/app/components/ayah-list-renderer/header/header.component.ts
+++ src/app/components/ayah-list-renderer/header/header.component.ts
@@ -1,37 +1,23 @@
-import { Component, Input, OnDestroy } from '@angular/core';
-import { BehaviorSubject, timer } from 'rxjs';
+import { Component, Input } from '@angular/core';
 import { NavigationService } from 'src/app/services/navigation.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'read-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class ReadHeaderComponent implements OnDestroy {
+export class ReadHeaderComponent {
   constructor(public navigator: NavigationService) {}
 
   @Input() menuList: any;
   @Input() currentMenuItemIndex: number;
 
-  private fadeSubscription: Subscription;
-
   public menuOpen = false;
-  public faded = new BehaviorSubject(false);
 
   async navigateToHome(event: MouseEvent) {
     if (!event.shiftKey && !event.ctrlKey) {
       event.preventDefault();
-      timer(100).subscribe(() => this.navigator.navigateToHome());
+      this.navigator.navigateToHome()
     }
-
-    if (!this.faded.value) {
-      this.faded.next(true);
-      this.fadeSubscription = timer(150).subscribe(() => this.faded.next(false));
-    }
-  }
-
-  ngOnDestroy() {
-    this.fadeSubscription?.unsubscribe();
   }
 }
