a820639897e9537c4a4ea7943cf304871e92b99e
AbdulSamadMalik <samadmalik542762@gmail.com>
2021-10-19 09:39:05 +0530

    Refactor base.js

--- public/base.js
+++ public/base.js
@@ -1,23 +1,21 @@
 var loadStartedAt = Date.now(),
+  themes = ['light-theme', 'dark-theme'],
   documentElement = document.documentElement,
-  storedTheme = localStorage.getItem('theme');
-
-documentElement.setAttribute('no-scroll', '');
-
-try {
-  var className = JSON.parse(storedTheme)['className'];
-  className && documentElement.classList.add(className);
-} catch (error) {
-  documentElement.classList.add('light-theme');
-}
+  storedTheme = localStorage.getItem('theme'),
+  isStandalone = matchMedia && matchMedia('(display-mode: standalone)').matches,
+  preferredTheme =
+    matchMedia && matchMedia('(prefers-color-scheme: dark)').matches ? themes[1] : themes[0];
 
 function removeSplash() {
-  var splashNodes = Array.from(document.querySelectorAll('.splash'));
-  for (var index = 0; index < splashNodes.length; index++) {
-    if (splashNodes[index]) {
-      splashNodes[index].parentElement.removeChild(splashNodes[index]);
+  var nodes = Array.from(document.querySelectorAll('.splash'));
+  if (!nodes.length) return;
+
+  for (var idx = 0; idx < nodes.length; idx++) {
+    if (nodes[idx] && nodes[idx].parentElement && nodes[idx].tagName !== 'SCRIPT') {
+      nodes[idx].parentElement.removeChild(nodes[idx]);
     }
   }
+
   window.dispatchEvent(new CustomEvent('splashcomplete'));
   window.removeEventListener('routeloaded', removeSplash);
   documentElement.removeAttribute('no-scroll');
@@ -29,13 +27,21 @@ function onRouteLoad() {
   setTimeout(removeSplash, timeGap);
 }
 
-function onLoadStart() {
-  // Remove Splash immediately when on PWA
-  if (window.matchMedia && window.matchMedia('(display-mode: standalone)').matches) {
-    removeSplash();
+function init() {
+  documentElement.setAttribute('no-scroll', 'true');
+
+  try {
+    var className = JSON.parse(storedTheme)['className'];
+    className && documentElement.classList.add(className);
+  } catch (error) {
+    documentElement.classList.add(preferredTheme);
   }
-  document.removeEventListener('DOMContentLoaded', onLoadStart);
+
+  // Remove Splash immediately when is PWA (Standalone)
+  isStandalone && removeSplash();
 }
 
-window.addEventListener('loadstart', onLoadStart);
+document.addEventListener('DOMContentLoaded', init);
 window.addEventListener('routeloaded', onRouteLoad);
+
+init();
